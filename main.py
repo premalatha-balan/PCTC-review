bf7810f24f7c68263a6e36ecf405895cff1656a9
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2023-02-28 17:05:59 +0000

    q9 - crazy code. Not working.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def findPattern (in_lst):
         temp1 = in_lst[i].find("1", temp0)
         p1_indx.append(temp1)
         temp0 = in_lst[i].find("0",temp1)
+        p1_indx.append(temp0-1)
         print(f"temp1 = {temp1} and temp0 ={temp0} ") 
     else:
       print(f"the entire string for {i} is {in_lst[i]} ")
@@ -30,7 +31,15 @@ def findPattern (in_lst):
   p1_indx.sort()
   print(f"rows in the pattern {row_indx}")
   print(f"columns in the pattern {p1_indx} ")
-  return(counter)
+  
+  out_pattern=in_lst[row_indx[0]:row_indx[-1]+1]
+  print(f"the pattern is {out_pattern} ")
+  for l in range(len(out_pattern)):
+    out_pattern[l]=out_pattern[l][p1_indx[0]:p1_indx[-1]+1]
+  #[p1_indx[0]:p1_indx[-1]]
+  print(f"the pattern is {out_pattern} ")
+  print(f"the pattern is {out_pattern} ")
+  return(out_pattern)
     
 
 firstgrid = [input() for i in range(10)]
@@ -43,5 +52,15 @@ subseq[3] = [input() for i in range(10)]
 subseq[4] = [input() for i in range(10)]
 
 #countf=findPattern(firstgrid)
-countf=findPattern(subseq[3])
-print(countf)
+pattern_grid=findPattern(firstgrid)
+print(f"the pattern grid is {firstgrid}")
+
+count_pattern = 0
+for i in range(5):
+  if pattern_grid == findPattern(subseq[i]): 
+    print(f"I am here at {i+1}")
+    count_pattern+=1
+    print(f"pattern matches for the grid {i+1} ")
+  else: print(f"pattern doesn't match for the grid {i+1} ")
+
+print(f"now of pattern matches is {count_pattern} ")
