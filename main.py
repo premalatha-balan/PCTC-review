6b45d8a47ce277b9378ec499dbedfece5082c9b9
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2023-03-05 07:53:59 +0000

    q10 done

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-X = "XXX"
-O = "OOO"
-
 
 def winning(win, p1, p2):
   X = "XXX"
@@ -8,7 +5,6 @@ def winning(win, p1, p2):
   if win == O: p1 += 1
   if win == X: p2 += 1
   return (p1, p2)
-
 
 column = lambda grid, i: [grid[k][i] for k in range(4)]
 
@@ -25,6 +21,7 @@ d0220 = lambda grid: [grid[k][0 - k - 2] for k in range(3)]
 d1331 = lambda grid: [grid[k + 1][0 - k - 1] for k in range(3)]
 
 grid = [["" for i in range(4)] for j in range(4)]
+
 grid_index = {"a": 0, "b": 1, "c": 2, "d": 3, "1": 3, "2": 2, "3": 1, "4": 0}
 
 play = [input() for i in range(16)]
@@ -74,3 +71,31 @@ p1, p2 = winning(win, p1, p2)
 print(f"second diag2 cut is {win} and p1 = {p1}, p2 = {p2} ")
 
 #Now count the other diags. There are four more of them.
+win = "".join(d0123(grid))[:-1]
+p1, p2 = winning(win, p1, p2)
+print(f"first diag11 cut is {win} and p1 = {p1}, p2 = {p2} ")
+win = "".join(d0123(grid))[1:]
+p1, p2 = winning(win, p1, p2)
+print(f"second diag11 cut is {win} and p1 = {p1}, p2 = {p2} ")
+
+win = "".join(d1032(grid))[:-1]
+p1, p2 = winning(win, p1, p2)
+print(f"first diag12 cut is {win} and p1 = {p1}, p2 = {p2} ")
+win = "".join(d1032(grid))[1:]
+p1, p2 = winning(win, p1, p2)
+print(f"second diag12 cut is {win} and p1 = {p1}, p2 = {p2} ")
+
+win = "".join(d0220(grid))[:-1]
+p1, p2 = winning(win, p1, p2)
+print(f"first diag21 cut is {win} and p1 = {p1}, p2 = {p2} ")
+win = "".join(d0220(grid))[1:]
+p1, p2 = winning(win, p1, p2)
+print(f"second diag21 cut is {win} and p1 = {p1}, p2 = {p2} ")
+
+win = "".join(d1331(grid))[:-1]
+p1, p2 = winning(win, p1, p2)
+print(f"first diag22 cut is {win} and p1 = {p1}, p2 = {p2} ")
+win = "".join(d1331(grid))[1:]
+p1, p2 = winning(win, p1, p2)
+print(f"second diag22 cut is {win} and p1 = {p1}, p2 = {p2} ")
+
